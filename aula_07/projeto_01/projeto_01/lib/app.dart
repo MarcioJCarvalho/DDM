f4883d18522d453b75c022353a8c3a5680b77976
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-03-23 22:06:24 -0300

    termino do projeto 01

---
+++ aula_07/projeto_01/projeto_01/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_01/home.dart';
+
+class App extends StatelessWidget{
+  Widget build (BuildContext context){
+    return MaterialApp(
+      title: 'Projeto 01',
+      home: Home(),
+    );
+  }
+}
---
+++ aula_07/projeto_01/projeto_01/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_01/app.dart';
+
+void main() {
+  runApp(App());
+}
